e6aae6dc5d7dfc6dd3a3ef9ad63ae3cfdd7a94d2
Andrew Jakubowicz <ajakubowicz@google.com>
2022-04-14 11:18:27 -0700

    Add the simple-carousel source code.

---
+++ build-it-with-lit/02-simple-carousel/src/constants.ts
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import { svg } from "lit";
+
+export const BOOTSTRAP_CHEVRON_RIGHT = svg`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#101423" class="bi bi-chevron-right" viewBox="0 0 16 16">
+ <path fill-rule="evenodd" stroke="#101423" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+ </svg>`;
+export const BOOTSTRAP_CHEVRON_LEFT = svg`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#101423" class="bi bi-chevron-left" viewBox="0 0 16 16">
+ <path fill-rule="evenodd" stroke="#101423" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+ </svg>`;
+
+const SLIDE_OUT_BACK_LEFT: Keyframe[] = [
+  { transform: "translateX(0)" },
+  { transform: "translateX(-100vw)" },
+];
+const SLIDE_OUT_BACK_RIGHT: Keyframe[] = [
+  { transform: "translateX(0)" },
+  { transform: "translateX(100vw)" },
+];
+const FORWARD_ANIMATION_OPTS: KeyframeAnimationOptions = {
+  duration: 500,
+  easing: "ease-in-out",
+  iterations: 1,
+};
+const REVERSE_ANIMATION_OPTS: KeyframeAnimationOptions = {
+  ...FORWARD_ANIMATION_OPTS,
+  direction: "reverse",
+};
+
+export type AnimationTuple = [Keyframe[], KeyframeAnimationOptions];
+
+export const SLIDE_LEFT_OUT: AnimationTuple = [
+  SLIDE_OUT_BACK_LEFT,
+  FORWARD_ANIMATION_OPTS,
+];
+export const SLIDE_RIGHT_OUT: AnimationTuple = [
+  SLIDE_OUT_BACK_RIGHT,
+  FORWARD_ANIMATION_OPTS,
+];
+export const SLIDE_LEFT_IN: AnimationTuple = [
+  SLIDE_OUT_BACK_LEFT,
+  REVERSE_ANIMATION_OPTS,
+];
+export const SLIDE_RIGHT_IN: AnimationTuple = [
+  SLIDE_OUT_BACK_RIGHT,
+  REVERSE_ANIMATION_OPTS,
+];
---
+++ build-it-with-lit/02-simple-carousel/src/slide-button.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import { LitElement, html, css } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("slide-button")
+class SlideButton extends LitElement {
+  static override styles = css`
+    #btn {
+      width: 2em;
+      height: 2em;
+      cursor: pointer;
+      border-radius: 1em;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      box-shadow: var(--shadow, gray) 0.2em 0.2em 0.4em,
+        var(--highlight, white) -0.1em -0.1em 0.2em;
+    }
+
+    #btn:active:hover,
+    #btn:hover:active {
+      box-shadow: inset var(--shadow, gray) 0.2em 0.2em 0.4em,
+        inset var(--highlight, white) -0.1em -0.1em 0.2em;
+    }
+
+    ::slotted(svg) {
+      width: 1em;
+      height: 1em;
+    }
+  `;
+
+  override render() {
+    return html` <div
+      id="btn"
+      tabindex="0"
+      role="button"
+      aria-pressed="false"
+      @keypress=${this.handleKeyPress}
+    >
+      <slot></slot>
+    </div>`;
+  }
+
+  handleKeyPress = () => {
+    this.dispatchEvent(new Event("click"));
+  };
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "slide-button": SlideButton;
+  }
+}
